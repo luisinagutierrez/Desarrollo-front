c29f82e5670f3ff80f6f982bfcd72467a0705399
luisinagutierrez <luisinagutierrez2618@gmail.com>
2023-08-23 14:17:33 -0300

    V08

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { CarouselModule } from 'ngx-bootstrap/carousel';
+import { CarouselModule } from 'ngx-bootstrap/carousel'; 
 
 //component
 import { AppRoutingModule } from './app-routing.module';
@@ -9,6 +9,7 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { BodyComponent } from './body/body.component';
 import { LoginComponent } from './login/login.component';
 import { FotterComponent } from './fotter/fotter.component';
+import { CarouselComponent } from './carousel/carousel.component';
 
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -20,8 +21,7 @@ import { MatIconModule } from '@angular/material/icon'; // icon
 import {MatGridListModule} from '@angular/material/grid-list';// columnas y filas
 import {MatFormFieldModule} from '@angular/material/form-field'; //contraseña
 import {MatInputModule} from '@angular/material/input';// login input 
-import {MatRadioModule} from '@angular/material/radio';
-import { CarouselComponent } from './carousel/carousel.component';// eleccion entre mayorita o minorista
+import {MatRadioModule} from '@angular/material/radio';/// eleccion entre mayorita o minorista
 
 @NgModule({
   declarations: [
