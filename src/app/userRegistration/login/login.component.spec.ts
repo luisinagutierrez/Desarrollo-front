b30a496040b5ba1a8cc8dc46fbd530208db40d31
luisinagutierrez <luisinagutierrez2618@gmail.com>
2025-02-05 15:36:17 -0300

    unit test login and sign up

--- src/app/userRegistration/login/login.component.spec.ts
+++ src/app/userRegistration/login/login.component.spec.ts
@@ -1,21 +1,55 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { LoginComponent } from './login.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ ReactiveFormsModule, HttpClientTestingModule, RouterTestingModule ],
       declarations: [LoginComponent]
-    });
+    }).compileComponents();
+
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
-
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  it('should validate email format', () => {
+    const emailControl = component.loginForm.controls['email'];
+    emailControl.setValue('invalid-email');
+    expect(emailControl.valid).toBe(false);
+    
+    emailControl.setValue('valid@email.com');
+    expect(emailControl.valid).toBe(true);
+  });
+
+  it('should validate form state', () => {
+    component.loginForm.controls['email'].setValue('');
+    component.loginForm.controls['password'].setValue('');
+    expect(component.loginForm.valid).toBe(false);
+
+    component.loginForm.controls['email'].setValue('test@email.com');
+    component.loginForm.controls['password'].setValue('password123');
+    expect(component.loginForm.valid).toBe(true);
+  });
+
+  it('should handle form submission', () => {
+    const loginSpy = jest.spyOn(component, 'login');
+  
+    component.loginForm.controls['email'].setValue('test@email.com');
+    component.loginForm.controls['password'].setValue('password123');
+    fixture.detectChanges();
+
+    component.login();
+    expect(loginSpy).toHaveBeenCalled();
+  });
 });
+
+
--- src/app/userRegistration/sign-up/sign-up.component.spec.ts
+++ src/app/userRegistration/sign-up/sign-up.component.spec.ts
@@ -1,15 +1,28 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { SignUpComponent } from './sign-up.component';
+import { FormsModule, NgForm } from '@angular/forms';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { UserService } from '../../services/user.service';
+import { CityService } from '../../services/city.service';
+import { ProvinceService } from '../../services/province.service';
 
 describe('SignUpComponent', () => {
   let component: SignUpComponent;
   let fixture: ComponentFixture<SignUpComponent>;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [SignUpComponent]
-    });
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [
+        FormsModule,HttpClientTestingModule,RouterTestingModule],
+      declarations: [ SignUpComponent ],
+      providers: [ 
+        UserService, CityService, ProvinceService
+      ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA]
+    }).compileComponents();
+
     fixture = TestBed.createComponent(SignUpComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -18,4 +31,23 @@ describe('SignUpComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should validate password format', () => {
+    expect(component.validatePassword('weak')).toBeFalsy();
+    expect(component.validatePassword('StrongPass1!')).toBeTruthy();
+  });
+
+  it('should toggle password visibility', () => {
+    expect(component.showPassword).toBeFalsy();
+    component.togglePasswordVisibility();
+    expect(component.showPassword).toBeTruthy();
+  });
+
+  it('should load provinces on init', () => {
+    const provinceService = TestBed.inject(ProvinceService);
+    const spy = jest.spyOn(provinceService, 'findAll');
+    component.ngOnInit();
+    expect(spy).toHaveBeenCalled();
+  });
 });
+
